3fd993c00e2412cac1bf449d55a9dc71d3dd0a37
Atharva Inamdar <aainamdar.cse@gmail.com>
2024-03-30 23:51:54 +0530

    about changed

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -11,29 +11,28 @@ const About = () => {
         <h2>About MGM's College Of Engineering, Nanded</h2>
         <div className="college">
           <img src={mgmclgImg} alt="Mgm" className="left" />
-          <p className="right">Mahatma Gandhi Mission, that started its journey in a modest way with a rural healthcare center at Nila and a hospital in Nanded, has emerged as a 5000-member family with more than 50 educational institutions, health-care centers and social-welfare units functioning under its umbrella at 5 centers i.e. Aurangabad, Nanded, Navi Mumbai, Noida and Parbhani. Spanning a period of 35 years, MGM establishments prove the deep faith of her management in the power of education that leads the society towards prosperity of the Nation. Mahatma Gandhi Mission has endeavored providing value-based education in all streams of knowledge like Engineering, Medicine, Dental, Nursing, Management, Law, Mass Communication & Journalism, Library Science, Computer Science, Biotechnology, Bioinformatics, Fine Art, Indian classical dance with state-of-the-art technology and infrastructure. Many new disciplines have also been included in the chain of establishments preserving the old. MGM’s University of Health Sciences has begun with exploring the new possibilities of imparting quality education. The founders of the trust are a group of ingenious Engineers, Doctors and Scientists having a high Career-profile and commitment to social interest. The Board of Management of the Trust is headed by Shri Kamalkishor N. Kadam, Member of Legislative Council of State of Maharashtra and Ex-minister of education.
-          </p>
+          <p className="right">MGM's College of Engineering, Nanded is one of the reputed institutions since 35 years. The institute caters for Under Graduate education for Civil, Computers, Mechanical, Electronics & Telecommunication, and Information Technology with four Post Graduate courses. The Research center in Mechanical and Computer Science Engineering provides the reinforcement for career building of engineers. The campus is completely Wi-fi enabled with State of art infrastructure and facilities. MGM's CoE Nanded, is the first NBA accredited institute in Marthwada region as well as it is a NAAC accredited institute.
+            The Department of Computer Science and Engineering, since its inception, has maintained a steady growth in every sphere of its activities and plays an important role in bringing a vibrant and forward looking academic environment within the University. The Department offers quality undergraduate course as well as provides an excellent infrastructure and facilities for research and consultancy. The Department is a university recognized research center for Ph.D. in Computer Science & Engineering.</p>
         </div>
 
         <h2>Department of Computer Science & Engineering</h2>
         <div className="college reverse">
           <p className="right">
-            The Department of Computer Science and Engineering was established
-            in 1984. The Department, since its inception, has maintained a
-            steady growth in every sphere of its activities and played an
-            important role in bringing a vibrant and Forward-looking academic
-            environment within the University. It is a premier institution
-            offering high quality undergraduate and postgraduate engineering
-            courses as well as providing an excellent infrastructure and
-            facilities for advanced research and consultancy. The Department of
-            Computer Science & Engineering strives for excellence in creating,
-            applying, and imparting knowledge in computer science and
-            engineering through comprehensive educational programs, research in
-            collaboration with industry and government, dissemination through
-            scholarly publications, and service to professional societies, the
-            community, the state, and the Nation. The Department has MoU with
-            IIT Bombay for Distance Education Program, Oracle Corp. for WDP
-            Program, IBM for PAP Program and Microsoft for Academic alliance.
+          The Department of Computer Science and Engineering was established in 1984. The
+ Department, since its inception, has maintained a steady growth in every sphere of its activities
+ and played an important role in bringing a vibrant and forward-looking academic environment.
+ Department is offering high quality undergraduate and postgraduate Engineering courses as
+ well as providing an excellent infrastructure and facilities for state of the art technical education
+ and research. The Department has research center of Swami Ramanand Teerth Marathwada
+ University, Nanded & Dr. Babasaheb Ambedkar Technological University, Lonere for Ph.D.
+ in CSE.  The Department has received research grants of Rs. 48.7 Lakhs from different funding
+ agencies such as BARC Mumbai, AICTE Delhi and AMUPMDC Mumbai.
+<br />
+ The Department of Computer Science & Engineering strives for excellence in creating,
+ applying, and imparting knowledge in Computer Science and Engineering through
+ comprehensive educational programs, research in collaboration with industry and government,
+ dissemination through scholarly publications, and service to professional societies, the
+ community, the State, and the Nation.
           </p>
           <img className="right" src={cse} alt="CSE" />
         </div>
