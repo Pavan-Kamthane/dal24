759c33f9990ef90eb91d056ca3942d3b67bce8e9
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2024-08-31 23:15:16 +0530

    footer improved

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,39 +1,25 @@
 import React from "react";
+import '../styles/footer.css'
 
-const footerStyles = {
-  background: "linear-gradient(45deg, #6d53ad, #5a3d99, #6d53ad)",
-  color: "white",
-  textAlign: "center",
-  padding: "10px",
-  fontWeight: "bold",
-  height:  "76px",
-};
 
-const link = {
-  color: "white",
-  textDecoration: "none",
-  padding:  "0 10px",
-  marginTop:  "-5px",
-
-};
 
 function Footer() {
   return (
-    <div style={footerStyles}>
+    <div className="footer">
       <p>&copy; 2024 All rights reserved | DAL 2024 | MGMCOE, Nanded</p>
       <p>
         Created By:{" "}
-        <a style={link} href="https://in.linkedin.com/in/pavankamthane">
-          Pavan Kamthane
+        <a  href="https://in.linkedin.com/in/pavankamthane">
+          Pavan Kamthane,
         </a>
-        , 
-        <a style={link} href="https://in.linkedin.com/in/atharva-inamdar">
-        Atharva Inamdar  
-        </a>,
-        <a style={link} href="https://in.linkedin.com/in/vedant-kahalekar">
-        Vedant Kahalekar
-        </a>,
-        <a style={link} href="https://in.linkedin.com/in/pranav-kalaskar-588a1a233">
+        
+        <a  href="https://in.linkedin.com/in/atharva-inamdar">
+        Atharva Inamdar,
+        </a>
+        <a  href="https://in.linkedin.com/in/vedant-kahalekar">
+        Vedant Kahalekar,
+        </a>
+        <a  href="https://in.linkedin.com/in/pranav-kalaskar-588a1a233">
         Pranav Kalaskar
         </a>
       </p>
