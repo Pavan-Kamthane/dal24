f62aac6e9bd06190f493c47767a73b5c22d3263b
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2024-08-31 21:58:50 +0530

    Marquee title changed

--- src/component/Marquee.jsx
+++ src/component/Marquee.jsx
@@ -10,13 +10,13 @@ const Marquee = () => {
       <div className="marquee">
         
         <a href={callForpaper1} download="Call_for_Papers_1.jpg" target="_blank" rel="noopener noreferrer">
-          Call for Papers 1
+          Call for Papers
         </a>
         <a href={callForpaper2} download="Call_for_Papers_2.jpg" target="_blank" rel="noopener noreferrer">
         {/* img */}
         <img src={newImg} alt="new" />
         
-          Call for Papers 2
+          Second Call for Papers
         </a>
         
       </div>
