734443ea4efb9805cde6e95a00bb7d21722a4f9c
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2024-09-01 12:45:55 +0530

    home page and site logo changed

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -5,7 +5,7 @@ import "../styles/home.css";
 import org1 from "../img/home/mgmlogo.jpeg";
 import org2 from "../img/home/mysoore logo.jpg";
 // import bg3 from "../img/BG3.jpeg";
-import ImageGallery from "react-image-gallery";
+// import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import Marquee from "../component/Marquee";
 
@@ -14,7 +14,7 @@ const Home = () => {
     <>
       <Marquee />
       <div className="banner">
-        <img src={bgMain} alt="Image Will Load soon" />
+        <img src={bgMain} alt=" Will Load soon" />
       </div>  
       <div className="about">
         <h2>About Conference</h2>
@@ -25,8 +25,7 @@ const Home = () => {
               (DAL' 24), is jointly organized by Department of CSE, MGM’s COE,
               Nanded (Maharashtra) and Department of Studies in Computer
               Science, University of Mysore, Mysore, Karnataka, India. It will
-              be held at MGM’s COE, Nanded on 30<sup>th</sup>
-              and 31<sup>st</sup> December 2024. DAL' 24 aims to provide a
+              be held at MGM’s COE, Nanded on 27, 28 and 29<sup>th</sup> December 2024. DAL' 24 aims to provide a
               platform for sharing innovative ideas, research stands as a
               distinguished annual conference dedicated to the realms of Data
               Analytics and Artificial Intelligence. Renowned for its
